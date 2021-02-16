84eac705b4425065e2c60312bd3d5c0670fd2303
Edson <edson.tolazzi@universo.univates.br>
2021-02-16 01:37:59 +0000

    Conversor de números romanos para decimal em C

---
+++ main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+  char num[] = "D";
+
+  char um = 'I';
+  char cinco = 'V';
+  char dez = 'X';
+  char cinq = 'L';
+  char cem = 'C';
+  char quin = 'D';
+  char mil = 'M';
+
+  int um1 = 1;
+  int cinco1 = 5;
+  int dez1 = 10;
+  int cinq1 = 50;
+  int cem1 = 100;
+  int quin1 = 500;
+  int mil1 = 1000;
+
+  char nulo;
+  int i = 0;
+  int x[10];
+  int res;
+
+  while (nulo != NULL) {
+    nulo = num[i];
+    i++;
+  }
+  i--;
+  
+  for (int j; j < i; j++) {
+    if (num[j] == um) {
+      x[j] = um1;
+    }
+    if (num[j] == cinco) {
+      x[j] = cinco1;
+    }
+    if (num[j] == dez) {
+      x[j] = dez1;
+    }
+    if (num[j] == cinq) {
+      x[j] = cinq1;
+    }
+    if (num[j] == cem) {
+      x[j] = cem1;
+    }
+    if (num[j] == quin) {
+      x[j] = quin1;
+    }
+    if (num[j] == mil) {
+      x[j] = mil1;
+    }
+  }
+
+  res = x[i];
+
+  while (i != 0) {
+    if(x[i] > x[i-1]) {
+      res = res - x[i-1];
+    }
+    if(x[i] < x[i-1]) {
+      res = res + x[i-1];
+    }
+    if(x[i] == x[i-1]) {
+      res = res + x[i-1];
+    }
+
+    i--;
+  }
+
+  printf("%d\n", i);
+  printf("%d\n", res);
+  
+  return 0;
+
+}
